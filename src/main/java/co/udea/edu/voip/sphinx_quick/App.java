9dcb45be51927ab64cb389ad6920d748241567a3
aareiza <aareiza@tredasolutions.com>
2016-05-04 20:51:06 -0500

    First Maven Project to Sphinx

---
+++ src/main/java/co/udea/edu/voip/sphinx_quick/App.java
@@ -0,0 +1,13 @@
+package co.udea.edu.voip.sphinx_quick;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
